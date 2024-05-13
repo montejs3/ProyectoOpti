629ec75e4e48fa5a239c87c63f0b793d51519b53
montejs3 <98852422+montejs3@users.noreply.github.com>
2024-05-13 10:36:36 -0500

    Creoq que esta melo

--- fase2.py
+++ fase2.py
@@ -43,3 +43,96 @@ d= {i: cirugias['Duración (en horas)'][i] for i in C}
 cirugias = pd.read_excel(file_name, sheet_name='Tabla 1',index_col=0)
 n= {i: cirugias['ID del doctor'][i] for i in C}
 
+# -------------------------------------
+# Creación del objeto problema en PuLP
+# -------------------------------------
+
+m = lp.LpProblem("Horarios_salas", lp.LpMinimize)
+
+# -----------------------------
+# Variables de Decisión
+# -----------------------------
+
+#Variable de decisión de horario de inicio de la cirugía
+x= {(i,j,k): lp.LpVariable(f"x_{i}_{j}_{k}", 0, None, lp.LpBinary) for i in C for j in F for k in S}
+
+#Varibale de decisión de si la cirugía se esta realizado 
+y= {(i,j,k): lp.LpVariable(f"y_{i}_{j}_{k}", 0, None, lp.LpBinary) for i in C for j in F for k in S}
+
+z = lp.LpVariable("z", 0, None, lp.LpContinuous)
+
+# -----------------------------
+# Restricciones
+# -----------------------------
+
+#Cada cirugía se realiza una vez
+for i in C:
+    m += lp.lpSum(x[i,j,k] for j in F for k in S) == 1
+    
+#Cada cirugía debde durar el tiempo que se indica
+for i in C:
+    m += lp.lpSum(y[i,j,k] for j in F for k in S) == d[i]
+    
+#Cada sala y cada franja horaria solo se puede ocupar por una cirugía
+for j in F:
+    for k in S:
+        m += lp.lpSum(y[i,j,k] for i in C) <= 1
+
+# cada cirugia  se presenta en franjas consecutivas y no presenta durante franjas previas a la franja de inicio y en la misma sala.
+for i in C:
+    for j in F:
+        for k in S:
+            if j+d[i] <= 17:
+                m +=lp.lpSum(y[i,u,k] for u in ( e for e in range(j, j+d[i])))  >=  x[i,j,k]*d[i]
+            
+
+# Ninguna cirugia puede iniciar si no se puede terminar 
+for i in C:
+    for j in F:
+        for k in S:
+            if j+d[i] > 17:
+                m +=   x[i,j,k] == 0
+
+# Ninguna cirugia se puede realizar si el doctor no esta disponible
+for i in C:
+    for j in F:
+        for k in S:
+            m += y[i,j,k] <= p[n[i],j]
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+# -----------------------------
+# Función objetivo
+# -----------------------------
+
+m += z
+
+# Optimizar modelo
+# -----------------------------
+# Optimizar el modelo con CBC (default de PuLP)
+m.solve()
+
+
+# -----------------------------
+#    Imprimir resultados
+# -----------------------------
+
+# Imprimir variables de decisión
+
+                
+for i in C:
+    for j in F:
+        for k in S:
+            if y[i,j,k].value() == 1:
+                print(f"Cirugía {i} en franja {j} en sala {k}")
